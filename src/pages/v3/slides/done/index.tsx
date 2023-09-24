9093bad62de69789e40f4d1e4612ddd29f094628
lawkelvin33 <lawkelvin33@korea.ac.kr>
2023-09-23 23:37:57 -0400

    simple-bot

--- src/pages/v3/slides/done/index.tsx
+++ src/pages/v3/slides/done/index.tsx
@@ -4,7 +4,7 @@ import { Wrapper } from "pages/v3/sections/Wrapper";
 import { Step } from "pages/v3/step.enum";
 import { useV3Selection } from "states/v3-global.states";
 import { useState } from "react";
-import { Redirect } from "react-router-dom"; // 이거 없다는데 머지
+import { useNavigate } from "react-router-dom"; // 이거 없다는데 머지
 
 export const Done = () => {
   const [investId] = useV3Selection(Step.Investment);
@@ -18,26 +18,21 @@ export const Done = () => {
   const uniswap = `https://app.uniswap.org/pools/${tokenId}`;
 
   const [isOn, setIsOn] = useState(false);
-  const [redirect, setRedirect] = useState(false);
+  const navigate = useNavigate();
 
   const handleToggle = () => {
     setIsOn(!isOn);
     if (!isOn) {
       // Redirect to a certain URL when turning the button on
-      setRedirect(true);
+      navigate('/your-specific-url');
     }
   };
 
-  // Define the URL to redirect to when the button is turned on
-  const redirectUrl = 't.me/invest_alarm_bot';
-
   return (
     <Wrapper>
-      <p>자경이가 여기에 텔레그램 관련된 내용 추가하면 됩니다~</p>
       <div>
       <h1>Toggle Button</h1>
       <button onClick={handleToggle}>{isOn ? 'Turn Off' : 'Turn On'}</button>
-      {redirect && <Redirect to={redirectUrl} />}
     </div>
 
       <a target="_blank" href={explorer}>
